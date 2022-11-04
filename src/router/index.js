78d6cae4637c12c65d1e2169b35e0e3bb0cd16f2
Romario Torres <cs360.x@hotmail.com>
2022-11-04 15:48:35 -0600

    pantalla de login

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '@/views/Login.vue'
+import Principal from '@/views/Principal.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '*',
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/principal',
+    name: 'Principal',
+    component: Principal,
+    // meta: {
+    //   autentificado: true
+    // }
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="contenedor_login">
+        <div class="cuadro_login">
+            <div class="contenedor_logo">
+                <img src="@/assets/2629.png" style="width: 100%;" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+
+<style>
+    .contenedor_login{
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+        .cuadro_login{
+            width: 300px;
+            height: 600px;
+            border: 1px solid black;
+        }
+</style>
